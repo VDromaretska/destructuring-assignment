9f8b292edf43a5824153299f1357911f69df8c29
VDromaretska <viktoriya.dromaretska@gmail.com>
2023-06-22 14:12:09 +0200

    Did challenge 0a, 0b

--- src/challenges/0a-array-destructuring.ts
+++ src/challenges/0a-array-destructuring.ts
@@ -1,9 +1,8 @@
 const mealsInDay = ["Breakfast", "Lunch", "Dinner", "Supper"];
 
 // TODO: use array destructuring
-const firstMeal = mealsInDay[0];
-const secondMeal = mealsInDay[1];
-const thirdMeal = mealsInDay[2];
+const [firstMeal, secondMeal, thirdMeal] = mealsInDay;
+console.log(firstMeal, secondMeal, thirdMeal);
 
 // export so the values can be tested - but you can ignore this
 export { mealsInDay, firstMeal, secondMeal, thirdMeal };
